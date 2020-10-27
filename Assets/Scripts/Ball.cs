d4d63810e1332d5b8fabc8498f90bfa2c9e1509a
Murad <krekotnev@gmail.com>
2020-10-27 20:53:04 +0300

    Lesson 9

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        print("qwe");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("trigger");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        print("trigger exit");
+    }
+}
