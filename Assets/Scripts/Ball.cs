4753c0d96d8fc782c541c7a8838c5edad26e5fa1
Murad <krekotnev@gmail.com>
2020-10-29 20:53:46 +0300

    Lesson 10

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float speed;
+    public Rigidbody2D rb;
+    bool isStarted;
+    public Pad pad;
+
+    private void Update()
+    {
+        if (isStarted)
+        {
+
+        }
+        else
+        {
+            Vector3 padPosition = pad.transform.position;
+
+            Vector3 ballNewPosition = new Vector3(padPosition.x, padPosition.y + 1, 0);
+            transform.position = ballNewPosition;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartBall();
+            }
+        }
+
+    }
+
+    private void StartBall()
+    {
+        Vector2 force = new Vector2(1, 1) * speed;
+        rb.AddForce(force);
+        isStarted = true;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        print("qwe");
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("trigger");
+
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        print("trigger exit");
+
     }
 }
---
+++ Assets/Scripts/Pad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pad : MonoBehaviour
+{
+    float yPosition;
+    void Start()
+    {
+        yPosition = transform.position.y;
+    }
+
+    void Update()
+    {
+        Vector3 mousePixelPosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePixelPosition);
+
+
+        Vector3 padNewposition = mouseWorldPosition;
+        padNewposition.z = 0;
+        padNewposition.y = yPosition;
+
+
+        transform.position = padNewposition;
+    }
+}
